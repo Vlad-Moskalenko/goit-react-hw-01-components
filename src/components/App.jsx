d11e275324e375b0bb4deaef05a49097a8b9f1a8
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-14 13:12:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendsList } from './FriendsList/FriendsList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from '../data/user';
-import data from '../data/data';
-import friends from '../data/friends';
-import transactions from '../data/transactions';
+import user from 'data/user';
+import data from 'data/data';
+import friends from 'data/friends';
+import transactions from 'data/transactions';
 
 export const App = () => {
   return (
