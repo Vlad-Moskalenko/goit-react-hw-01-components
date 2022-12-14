f1420c22790c2976e1b4bf3296b9fbf2457a815b
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-14 13:30:12 +0200

    update3

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendsList } from './FriendsList/FriendsList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import user from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+
+export const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
