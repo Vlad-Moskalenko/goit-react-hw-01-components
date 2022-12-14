7b68a54b525358b9f3d45ec61877e5c5d10689b9
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-14 19:14:26 +0200

    fix: add cssModuleStyles for Profile

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,8 +1,14 @@
 import css from './StatisticsItem.module.css';
 import PropTypes from 'prop-types';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 export const StatisticsItem = ({ statData: { label, percentage } }) => (
-  <li className={css.item}>
+  <li style={{ backgroundColor: getRandomHexColor() }} className={css.item}>
     <span className="label">{label}</span>
     <span className="percentage">{percentage}%</span>
   </li>
